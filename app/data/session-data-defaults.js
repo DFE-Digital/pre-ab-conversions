de5aa27e3ab1b9a0476b0a40bd629c28104935a1
Maeve Roseveare <68232608+MaeveRoseveare@users.noreply.github.com>
2021-02-23 18:07:53 +0000

    Updated js file for conflicts

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -19,8 +19,17 @@ Example usage:
 
 module.exports = {
 
-  "opening-date": "Tuesday 21 March 2021",
-  "aorequired": "Yes",
-  "project-rationale": "St Wilfred’s is a small primary school in Warrington which is facing a financially challenging future should it remain on its own. Low pupil numbers and the high level of staffing costs due to the school  being supported by an Agency Interim Head Teacher,  contributed to this. The Chair St Wilfred’s and Dynamics trust have agreed to a Memorandum of Understanding which will allow Dynamics Trust to oversee all operational aspects of the school with immediate effect. Central services provided will include school improvement, finance and operational support and HR support. Dynamics Trust have also appointed Gareth Smith, Head Teacher of Angel Primary School, to the role of Interim Executive Head Teacher."
+/* overview */
+"opening-date": "Tuesday 21 March 2021",
+"aorequired": "Yes",
+
+/*rationale */
+"project-rationale": "St Wilfred’s is a small primary school in Warrington which is facing a financially challenging future should it remain on its own. Low pupil numbers and the high level of staffing costs due to the school  being supported by an Agency Interim Head Teacher,  contributed to this. The Chair St Wilfred’s and Dynamics trust have agreed to a Memorandum of Understanding which will allow Dynamics Trust to oversee all operational aspects of the school with immediate effect. Central services provided will include school improvement, finance and operational support and HR support. Dynamics Trust have also appointed Gareth Smith, Head Teacher of Angel Primary School, to the role of Interim Executive Head Teacher.",
+
+  /* general info */
+"viability": "Yes",
+"deficit": "No",
+"diocesan": "Yes",
+"distance": "20.1 miles"
 
 }
