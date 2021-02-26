182f17edc6152917c8790e45f532c853b18c55da
TRACY MARSHALL <tracy.marshall@education.gov.uk>
2021-02-26 12:15:48 +0000

    Back button renamed, data pushed through to input pages

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Academy conversions',
+  serviceName: 'Manage trusts ans academy schoolss',
 
   // Default port that prototype runs on
   port: '3000',
