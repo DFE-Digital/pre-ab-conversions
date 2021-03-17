1be891c1df7f55c1d4f46752897b83400d6b8c60
NOURIACH <NOURIACH@LTWT618415>
2021-03-17 12:24:09 +0000

    Clear up code presentation

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -22,11 +22,12 @@ module.exports = {
   /* overview */
   "opening-date": "Monday 1 March 2021",
   "aorequired": "Yes",
+  "test-name": "Nathan",
 
   /*rationale */
-"project-rationale": "St Wilfrid’s is a small primary school in Warrington which is facing a financially challenging future should it remain on its own. Low pupil numbers and the high level of staffing costs due to the school  being supported by an Agency Interim Head Teacher,  contributed to this. The Chair St Wilfrid’s and Dynamics trust have agreed to a Memorandum of Understanding which will allow Dynamics Trust to oversee all operational aspects of the school with immediate effect. Central services provided will include school improvement, finance and operational support and HR support. Dynamics Trust have also appointed Gareth Smith, Head Teacher of Angel Primary School, to the role of Interim Executive Head Teacher.",
+  "project-rationale": "St Wilfrid’s is a small primary school in Warrington which is facing a financially challenging future should it remain on its own. Low pupil numbers and the high level of staffing costs due to the school  being supported by an Agency Interim Head Teacher,  contributed to this. The Chair St Wilfrid’s and Dynamics trust have agreed to a Memorandum of Understanding which will allow Dynamics Trust to oversee all operational aspects of the school with immediate effect. Central services provided will include school improvement, finance and operational support and HR support. Dynamics Trust have also appointed Gareth Smith, Head Teacher of Angel Primary School, to the role of Interim Executive Head Teacher.",
 
-    /* general info */
+  /* general info */
   "viability": "Yes",
   "deficit": "There are no financial issues at the school",
   "diocesan": "Yes",
