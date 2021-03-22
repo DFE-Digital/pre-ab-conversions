f343e045bcdc7729fc5b21322f152448b5675ec4
TRACY MARSHALL <tracy.marshall@education.gov.uk>
2021-03-22 11:53:29 +0000

    Bugs fixed
    Top margin changed on projects page
    Tags commented out
    Duplicate button replaced on summary
    Cancel buttons changed to use javascript

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -21,7 +21,7 @@ module.exports = {
 
   /* overview */
   "opening-date": "Monday 1 March 2021",
-  "aorequired": "Yes",
+
 
   /*rationale */
   "project-rationale": "St Wilfrid’s is a small primary school in Warrington which is facing a financially challenging future should it remain on its own. Low pupil numbers and the high level of staffing costs due to the school  being supported by an Agency Interim Head Teacher,  contributed to this. The Chair St Wilfrid’s and Dynamics trust have agreed to a Memorandum of Understanding which will allow Dynamics Trust to oversee all operational aspects of the school with immediate effect. Central services provided will include school improvement, finance and operational support and HR support. Dynamics Trust have also appointed Gareth Smith, Head Teacher of Angel Primary School, to the role of Interim Executive Head Teacher.",
