7f8faaf1abc5ac8ef01dbe1f315672e02158677b
TRACY MARSHALL <tracy.marshall@education.gov.uk>
2021-04-22 09:41:17 +0100

    UR changes

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -22,6 +22,9 @@ module.exports = {
   /* overview */
   "opening-date": "Monday 1 March 2021",
 
+  /* Academy order required*/
+  "aorequired": "Yes",
+
 
   /*rationale */
   "project-rationale": "The Diocesan Academy Policy (December 2019) states the expectation that all schools within the Diocese of Warrington will convert to academy status within one of the four Diocesan Catholic Education Trusts by July 2022. The resulting growth plan earmarks our school for transition into Dynamics Trust. This Catholic trust model ensures every school has its place and no individual school is left isolated or vulnerable, in a rapidly changing environment. It also ensures that the uniqueness of each individual school is celebrated and the unique contribution of each is recognised and appreciated. The Trust has an experienced Director of Learning and Teaching who effectively co-ordinates some central school effectiveness provision to monitor school performance/ achievement and ensures support is provided and matched to need, mainly as brokerage, within the system leadership structure.The Trust’s school improvement model is based on the new Northern Alliance, aiming to build capacity and improve educational standards in the North of England. In the words of Sir David Carter “the Northern Alliance is an innovative partnership which illustrates the strength of collaboration that is emerging as part of our developing self-improving school system.” Through the excellent, collaborative work delivered to date and first-hand experience of supporting other schools/academies across the North West, the expanding Trust has the expertise to ensure that each partner school can benefit, as a robust infrastructure to support future growth is built. Careful monitoring of each school by the CEO, School Improvement Partner, Headteachers and Trust Board ensures that high quality teaching takes place across the Trust. There is currently excellent practice demonstrated within the Primary and Secondary partners within the existing Dynamics Trust.",
@@ -29,13 +32,13 @@ module.exports = {
   /* general info */
   "deficit": "There are no financial issues at the school",
   "diocesan": "Yes",
-  "distance": "20.1"
+  "distance": "20.1",
 
   /* finance data */
-  "finance-following-year-2021" : "0.00",
-  "finance-following-year-2022" : "143,931.00",
-  "finance-forward-2021" : "0.00",
-  "finance-forward-2022" : "0.00"
+  "finance-current-year-2021": "£0.00",
+  "finance-following-year-2022": "£143,931.00",
+  "finance-forward-2021": "£0.00",
+  "finance-forward-2022": "£0.00"
 
 }
 
