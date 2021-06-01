2000efec260fc066ebd8b9db6c33ca46deec4fcf
TRACY MARSHALL <tracy.marshall@education.gov.uk>
2021-06-01 16:30:43 +0100

    Rejected route added

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,21 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+//sending users to different pages
+// Run this code when a form is submitted to 'juggling-balls-answer'
+router.post('/status-answer', function (req, res) {
+
+    // Make a variable and give it the value from 'juggling-balls'
+    var jugglingBalls = req.session.data['status']
+  
+    // Check whether the variable matches a condition
+    if (jugglingBalls == "Approved"){
+      // Send user to next page
+      res.redirect('/status/approved')
+    } else {
+      // Send user to ineligible page
+      res.redirect('/ineligible')
+    }
+  
+  })
