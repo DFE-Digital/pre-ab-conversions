22544931f965135af0775791f240bc4be87c9b3a
Anne Chan <anneychan209@gmail.com>
2022-05-23 15:32:30 +0100

    update to latest

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Manage an academy conversion',
+  serviceName: 'testing Prepare for advisory board',
 
   // Default port that prototype runs on
   port: '3000',
