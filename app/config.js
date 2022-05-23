8681ea49237d1d3d31dd5c464487e79d7b4fc68b
annechan209 <89071240+annechan209@users.noreply.github.com>
2022-05-23 15:07:10 +0100

    Revert "A cv1.0 12 may2022"

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Prepare for Advisory Board',
+  serviceName: 'Manage an academy conversion',
 
   // Default port that prototype runs on
   port: '3000',
