7964b163a9a88bda82103bed4412331d00661ce1
Ming Chan <mingosoft@gmail.com>
2022-07-21 15:27:19 +0100

    adding sprint 36

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,59 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 //***********
+//* SPRINT 36
+//***********
+
+router.post('/sprint-36/status/approved/conditions-answer', function(req, res) {
+
+  const approvedCondition = req.body["approved-conditions"];
+
+  if (approvedCondition == "Yes"){
+    // Send user to next page
+    res.redirect('conditions_yes');
+  } 
+  else if (approvedCondition == "No") {
+    res.redirect('dates');
+  }
+
+});
+
+router.post('/sprint-36/status/status-answer', function(req, res) {
+
+  const projectStatus = req.body["decision-status"];
+
+  if (projectStatus == "Declined"){
+    // Send user to next page
+    res.redirect('declined/who-made-this-decision')
+  } 
+  else if (projectStatus == "Deferred") {
+    res.redirect('deferred/who-made-this-decision')
+  }
+  else if (projectStatus == "Approved"){
+    res.redirect('approved/who-made-this-decision')
+  }
+    
+});
+
+router.post('/sprint-36/type-answer', function (req, res) {
+  // Make a variable to give it the value from the radio buttons on the index page  
+  var Task = req.session.data['control-name']
+
+  // Check whether the variable matches a condition
+  if (Task == "conversions") { 
+    // Send user to next page 
+    res.redirect('projects-list')
+
+  }   
+  else if (Task == "transfers") {
+    //send user to transfers prototype
+    res.redirect('https://pre-ab-transfers.herokuapp.com/version-4/dashboard-home')
+  }
+
+})
+
+
+//***********
 //* SPRINT 35
 //***********
 
