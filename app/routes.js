88c6ffb94d50efd5f838d4e4295da06fb626b0f0
Ming Chan <mingosoft@gmail.com>
2022-10-03 15:23:44 +0100

    adding sprint 41

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,11 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 //***********
+//* SPRINT 41
+//***********
+require('./routes/sprint-41/routes.js')(router);
+
+//***********
 //* SPRINT 40
 //***********
 require('./routes/sprint-40/routes.js')(router);
