374681591255876c7188506cf36ed74300a0af53
Iain MacDonald <75545992+IainMacDonaldUX@users.noreply.github.com>
2023-03-20 08:24:58 +0000

    Sprint 52 setup

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,11 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 //***********
+//* SPRINT 52
+//***********
+require('./routes/sprint-52/routes.js')(router);
+
+//***********
 //* SPRINT 51
 //***********
 require('./routes/sprint-51/routes.js')(router);
